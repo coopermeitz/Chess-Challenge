f7db049cb0f23bff2451257b53e55704f1f9664d
Cooper Meitz <coopermeitz@gatech.edu>
2023-07-27 19:14:51 -0400

    minimax skeleton

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,14 +1,42 @@
 ﻿using ChessChallenge.API;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 public class MyBot : IChessBot
 {
-    public static Move GetRandomMove(Board board)
+    /// <summary>
+    /// Returns a random move.
+    /// </summary>
+    /// <param name="board">The <see cref="Board"/>.</param>
+    /// <returns>A random legal move.</returns>
+    private static Move GetRandomMove(Board board)
     {
         Move[] moves = board.GetLegalMoves();
         return moves[218372381273 % moves.Length];  
     }
     public Move Think(Board board, Timer timer)
+    {
+        return board.IsWhiteToMove ? White(board, timer, 5) : Black(board, timer, 5);
+    }
+
+    /// <summary>
+    /// Uses heuristic(s) to analyze a board without thinking ahead.
+    /// </summary>
+    /// <param name="board">The current state of the game.</param>
+    /// <returns>A double where:
+    /// x > 0: white is estimated to be better
+    /// x = 0: the game is estimated to be a draw
+    /// x < 0: black is estimated to be better
+    ///</returns>
+    private static double AnalyzeBoard(Board board)
+    {
+        return 0;
+    }
+
+    private Move Black(Board board, Timer timer, int depth)
+    {
+        return White(board, timer, depth - 1);
+    }
+
+    private Move White(Board board, Timer timer, int depth)
     {
         return GetRandomMove(board);
     }
